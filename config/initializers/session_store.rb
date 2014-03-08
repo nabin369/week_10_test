da27e6ebcd77ab6601e1602a80158e05768c97c7
nabin369 <nabinrai369@gmail.com>
2014-03-08 17:10:45 +0000

    initial commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Week10Test::Application.config.session_store :cookie_store, key: '_week_10_test_session'
---
+++ features/step_definitions/user_steps.rb
@@ -0,0 +1,31 @@
+Given(/^I am on the homepage$/) do
+  visit root_path
+end
+
+Then(/^I should see description of the apps$/) do
+  message = 'logged into'
+  expect(page).to have_content(message)
+end
+
+# When(/^I click "(.*?)"$/) do |button|
+#   click_link button
+# end
+
+# Then(/^I should see the sign up page$/) do
+#   expect(page).to have_content("Sign Up")
+#   expect(page).to have_content("Username")
+#   expect(page).to have_content("Password")
+# end
+
+# Given(/^I am a registered user$/) do
+# visit new_user_path
+#   fill_in 'user[username]', with: 'test'
+#   fill_in 'user[password_digest]', with: 'test'
+#   click_button "Sign Up"
+# end
+
+# Then(/^I should see the sign in page$/) do
+#   expect(page).to have_content('Sign In')
+#   expect(page).to have_content("Username")
+#   expect(page).to have_content("Password")
+# end
